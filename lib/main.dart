fe5c4862405b6c0f9c88bdc90c0ac9f0fbd76243
Anthony Yap <yappanthony22@gmail.com>
2024-09-24 17:14:38 +0800

    remove container in footermsg

--- lib/main.dart
+++ lib/main.dart
@@ -45,31 +45,28 @@ class FooterMsg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        
-        children: [
-          Text(
-            "Don't have an account? ",
-            style: TextStyle(
-              fontWeight: FontWeight.w500
-            )
-          ),
-          Text(
-            "Register Now",
-            style: TextStyle(
-              color: Color.fromARGB(255, 10, 148, 134),
-              fontWeight: FontWeight.w600,
-              decoration: TextDecoration.underline,
-              decorationColor: Color.fromARGB(255, 10, 148, 134),
-              decorationThickness: 2
-            )
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      
+      children: [
+        Text(
+          "Don't have an account? ",
+          style: TextStyle(
+            fontWeight: FontWeight.w500
           )
-        ],
-      ),
+        ),
+        Text(
+          "Register Now",
+          style: TextStyle(
+            color: Color.fromARGB(255, 10, 148, 134),
+            fontWeight: FontWeight.w600,
+            decoration: TextDecoration.underline,
+            decorationColor: Color.fromARGB(255, 10, 148, 134),
+            decorationThickness: 2
+          )
+        )
+      ],
     );
   }
 }
